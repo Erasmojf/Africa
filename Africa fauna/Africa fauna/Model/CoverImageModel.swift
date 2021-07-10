cede6bf03b4ed7b9b4d6707e16bb7b8861220d18
Erasmo J.F Da Silva <erasmogohiper@gmail.com>
2021-07-10 16:59:00 -0300

    CM - Learn how to decode a JSON file and fetch data with SwiftUI
    Learn how to decode a JSON file and fetch data
    with SwiftUI

---
+++ Africa fauna/Africa fauna/Model/CoverImageModel.swift
@@ -0,0 +1,14 @@
+//
+//  CoverImageModel.swift
+//  Africa fauna
+//
+//  Created by Erasmo J.F Da Silva on 10/07/21.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+    
+}
